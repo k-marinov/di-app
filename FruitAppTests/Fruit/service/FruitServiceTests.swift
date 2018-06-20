491f4f5fb3e94c97055cc5658e783ff32ee5895f
keran.marinov <linxmap@gmail.com>
2018-06-20 20:57:02 +0100

    Added fruit service tests for http

---
+++ FruitAppTests/Fruit/service/FruitServiceTests.swift
@@ -0,0 +1,56 @@
+import XCTest
+import RxSwift
+
+@testable import FruitApp
+
+class FruitServiceTests: XCTestCase {
+
+    private let mocker: FruitHttpMocker = FruitHttpMocker()
+    private let service: FruitService = FruitService(componentCreatable: ComponentCreator())
+    private let disposeBag: DisposeBag = DisposeBag()
+
+    override func setUp() {
+        super.setUp()
+        mocker.setUpStubs()
+    }
+
+    override func tearDown() {
+        mocker.removeStubs()
+        super.tearDown()
+    }
+
+    func testFindAllFruits_whenSuccess_returnsFruits() {
+        FruitHttpMocker.scenario = FruitHttpMocker.Scenario.success
+        var fruits: [FruitResource] = [FruitResource]()
+
+        let expectation = self.expectation(description: "")
+        service.findAllFruits(with: FruitsRequest())
+            .subscribe(onNext: { newFruits in
+                fruits.append(contentsOf: newFruits)
+                expectation.fulfill()
+            }, onError: { error in
+                expectation.fulfill()
+            }).disposed(by: disposeBag)
+        wait(for: [expectation], timeout: Constants.timeout)
+
+        XCTAssertEqual(fruits.count, 9)
+    }
+
+    func testFindAllFruits_whenFails_returnsApiErrorClient() {
+        FruitHttpMocker.scenario = FruitHttpMocker.Scenario.fail
+        var apiError: ApiError?
+
+        let expectation = self.expectation(description: "")
+        service.findAllFruits(with: FruitsRequest())
+            .subscribe(onNext: { _ in
+                expectation.fulfill()
+            }, onError: { error in
+                apiError = error as? ApiError
+                expectation.fulfill()
+            }).disposed(by: disposeBag)
+        wait(for: [expectation], timeout: Constants.timeout)
+
+        XCTAssertEqual(apiError, ApiError.client)
+    }
+
+}
